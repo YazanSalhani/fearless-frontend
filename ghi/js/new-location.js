f07e276d59d1a1139e589b55bc1554396d4bef8b
Yazan <yzan.alsalhani@gmail.com>
2024-06-26 19:12:45 +0300

    added an api for state

---
+++ ghi/js/new-location.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', async () => {
+
+    const url = 'http://localhost:8000/api/states/';
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      const selectTag = document.getElementById('state');
+      for (let state of data.states) {
+        const option = document.createElement('option');
+        option.value = state.abbreviation;
+        option.innerHTML = state.state;
+        selectTag.appendChild(option);
+      }
+    }
+
+  });
